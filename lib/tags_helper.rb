36425e204704924df3d2096bf67c72ce86890aad
Martijn Lafeber <lafeber@gmail.com>
2009-05-06 15:43:03 +0200

    if tag appears double in combined tag cloud, sum its counts

--- lib/tags_helper.rb
+++ lib/tags_helper.rb
@@ -4,10 +4,18 @@ module TagsHelper
     return if tags.empty?
     
     max_count = tags.sort_by(&:count).last.count.to_f
+    max_count = 1 if max_count == 0
     
     tags.each do |tag|
-      index = ((tag.count / max_count) * (classes.size - 1)).round
+      index = ((double_count(tag, tags, tag.count) / (max_count)) * (classes.size - 1)).round
       yield tag, classes[index]
     end
+  end
+  
+  # This makes sure that multiple tags sum their count
+  def double_count(tag, tags, count)
+    doubles = tags.select{|t| t.name == tag.name}
+    return count if doubles.size < 2
+    doubles.map(&:count).sum
   end
 end
